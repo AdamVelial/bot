1502608612ff979e7a0ae46530626bcddb6c9615
armurtazin <armurtazin@ozon.ru>
2023-02-05 20:12:46 +0300

    Implements list of commands

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 
+	"github.com/AdamVelial/bot/internal/service/product"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/joho/godotenv"
 )
@@ -30,16 +31,50 @@ func main() {
 
 	updates := bot.GetUpdatesChan(u)
 
+	productService := product.NewService()
+
 	for update := range updates {
 		if update.Message == nil {
 			continue
 		}
 
-		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Your message: "+update.Message.Text)
-		// msg.ReplyToMessageID = update.Message.MessageID
-
-		bot.Send(msg)
+		switch update.Message.Command() {
+		case "help":
+			helpCommand(bot, update.Message)
+		case "list":
+			listCommand(bot, update.Message, productService)
+		default:
+			defualtBehaviar(bot, update.Message)
+		}
 	}
+}
+
+func listCommand(bot *tgbotapi.BotAPI, message *tgbotapi.Message, productService *product.Service) {
+	products := "List of product: \n\n"
+
+	for _, product := range productService.List() {
+		products += product.Title + "\n"
+	}
+
+	msg := tgbotapi.NewMessage(message.Chat.ID, products)
+	bot.Send(msg)
+}
+
+func helpCommand(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
+	helpMsg :=
+		`
+	/help - help
+	/list - list
+	`
+	msg := tgbotapi.NewMessage(message.Chat.ID, helpMsg)
+	bot.Send(msg)
+}
+
+func defualtBehaviar(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
+	log.Printf("[%s] %s", message.From.UserName, message.Text)
+
+	msg := tgbotapi.NewMessage(message.Chat.ID, "Your message: "+message.Text)
+	// msg.ReplyToMessageID = update.Message.MessageID
+
+	bot.Send(msg)
 }
