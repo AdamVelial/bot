c4bfbe10c17dec94668447c002f86e1e46b1dccd
armurtazin <armurtazin@ozon.ru>
2023-02-05 15:20:16 +0300

    add telegram api

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+const token = "token"
 
 func main() {
-	fmt.Println("hello")
+	bot, err := tgbotapi.NewBotAPI(token) // Telegram Token
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.UpdateConfig{
+		Timeout: 60,
+	}
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Your message: "+update.Message.Text)
+		// msg.ReplyToMessageID = update.Message.MessageID
+
+		bot.Send(msg)
+	}
 }
